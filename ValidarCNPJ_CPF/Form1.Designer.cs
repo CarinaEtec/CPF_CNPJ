751769b52762396b7a35ffbe3ec5422378ad433d
aluno etec <aluno@ETECPAOF.LAN>
2019-06-13 20:31:45 -0300

    first commit

---
+++ ValidarCNPJ_CPF/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ValidarCNPJ_CPF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.maskValor = new System.Windows.Forms.MaskedTextBox();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.rbtCPF = new System.Windows.Forms.RadioButton();
+            this.rbtCNPJ = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // maskValor
+            // 
+            this.maskValor.BackColor = System.Drawing.Color.White;
+            this.maskValor.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskValor.Location = new System.Drawing.Point(107, 65);
+            this.maskValor.Name = "maskValor";
+            this.maskValor.Size = new System.Drawing.Size(258, 37);
+            this.maskValor.TabIndex = 12;
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnValidar.Location = new System.Drawing.Point(139, 158);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(199, 34);
+            this.btnValidar.TabIndex = 11;
+            this.btnValidar.Text = "Validar";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click_1);
+            // 
+            // rbtCPF
+            // 
+            this.rbtCPF.AutoSize = true;
+            this.rbtCPF.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtCPF.Location = new System.Drawing.Point(237, 108);
+            this.rbtCPF.Name = "rbtCPF";
+            this.rbtCPF.Size = new System.Drawing.Size(68, 33);
+            this.rbtCPF.TabIndex = 9;
+            this.rbtCPF.TabStop = true;
+            this.rbtCPF.Text = "CPF";
+            this.rbtCPF.UseVisualStyleBackColor = true;
+            // 
+            // rbtCNPJ
+            // 
+            this.rbtCNPJ.AutoSize = true;
+            this.rbtCNPJ.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtCNPJ.Location = new System.Drawing.Point(151, 108);
+            this.rbtCNPJ.Name = "rbtCNPJ";
+            this.rbtCNPJ.Size = new System.Drawing.Size(86, 33);
+            this.rbtCNPJ.TabIndex = 8;
+            this.rbtCNPJ.TabStop = true;
+            this.rbtCNPJ.Text = "CNPJ";
+            this.rbtCNPJ.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(469, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Informe o número a validar e marque o seu tipo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 222);
+            this.Controls.Add(this.maskValor);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.rbtCPF);
+            this.Controls.Add(this.rbtCNPJ);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.MaskedTextBox maskValor;
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.RadioButton rbtCPF;
+        private System.Windows.Forms.RadioButton rbtCNPJ;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ValidarCNPJ_CPF/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValidarCNPJ_CPF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rbtCNPJ_CheckedChanged(object sender, EventArgs e)
+        {
+            maskValor.Text = "";
+            maskValor.Mask = "00,000,000/0000-00";
+            maskValor.MaskInputRejected += new MaskInputRejectedEventHandler(maskValor_MaskInputRejected);
+        }
+        private void rdb_cpf_CheckedChanged(object sender, EventArgs e)
+        {
+            maskValor.Text = "";
+            maskValor.Mask = "000,000,000-00";
+            maskValor.MaskInputRejected += new MaskInputRejectedEventHandler(maskValor_MaskInputRejected);
+        }
+
+        void maskValor_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            toolTip1.ToolTipTitle = "Número informado inválido";
+            toolTip1.Show("Somente digitos de (0-9) são permitidos.", maskValor, maskValor.Location, 5000);
+        }
+
+        private void btnValidar_Click_1(object sender, EventArgs e)
+        {
+            string valor = maskValor.Text;
+            if (rbtCNPJ.Checked)
+            {
+                if (ValidaCNPJ.IsCnpj(valor))
+                {
+                   MessageBox.Show("O número é um CNPJ Válido !");
+                }
+                else
+                {
+                    MessageBox.Show("O número é um CNPJ Inválido !");
+                }
+            }
+
+            else if (rbtCPF.Checked)
+            {
+                if (ValidaCPF.IsCpf(valor))
+                {
+                    MessageBox.Show("O número é um CPF Válido !");
+                }
+                else
+                {
+                    MessageBox.Show("O número é um CPF Inválido !");
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Informe o número e o seu tipo para validação !");
+            }
+        }
+    }
+}
